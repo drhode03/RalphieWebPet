4bdd3aa3c78eaf165d860b1da0c4cdb09f89fdab
drhode03 <123619542+drhode03@users.noreply.github.com>
2024-11-21 13:38:29 -0700

    Update site-pet.js

--- site-pet.js
+++ site-pet.js
@@ -45,7 +45,7 @@ function createSitePet(gfx) {
   };
 
   var update = () => {
-    let bgX = -64 * frame ;
+    let bgX = -64 * frame -10;
     let bgY = -64 * anim ;
     let pos = `${bgX}px ${bgY}px `;
     ele.style.backgroundPosition = pos;
